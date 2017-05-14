2e9d719f66ab39ef1fbbc95de28712b8bfbb870d
Rgl88 <delta4tesla@gmail.com>
2017-05-14 13:20:04 +0430

    gantt persian locale

---
+++ codebase/sources/locale/locale_fa.js
@@ -0,0 +1,57 @@
+/*
+@license
+
+dhtmlxGantt v.4.1.0 Stardard
+This software is covered by GPL license. You also can obtain Commercial or Enterprise license to use it in non-GPL project - please contact sales@dhtmlx.com. Usage without proper license is prohibited.
+
+(c) Dinamenta, UAB.
+*/
+gantt.locale = {
+  date: {
+    month_full: ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"],
+    month_short: ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"],
+    day_full: ["یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنجشنبه", "جمعه", "شنبه"],
+    day_short: ["ی", "د", "س", "چ", "پ", "ج", "ش"]
+  },
+  labels: {
+    new_task: "فعالیت جدید",
+    icon_save: "ذخیره",
+    icon_cancel: "لغو",
+    icon_details: "جزئیات",
+    icon_edit: "ویرایش",
+    icon_delete: "حذف",
+    confirm_closing: "",//تغییرات شما از بین خواهد رفت، آیا مطمئن هستید؟
+    confirm_deleting: "فعالیت مورد نظر به طور دائمی حذف خواهد شد، آیا مطمئن هستید؟",
+    section_description: "توضیحات",
+    section_time: "بازه زمانی",
+    section_type: "نوع",
+
+
+    /* grid columns */
+    column_text: "عنوان فعالیت",
+    column_start_date: "زمان شروع",
+    column_duration: "مدت",
+    column_add: "",
+
+    /* link confirmation */
+    link: "لینک",
+    confirm_link_deleting: "حذف خواهد شد",
+    link_start: " (شروع)",
+    link_end: " (پایان)",
+
+    type_task: "فعالیت",
+    type_project: "پروژه",
+    type_milestone: "مراحل",
+
+    minutes: "دقیقه ها",
+    hours: "ساعت ها",
+    days: "روزها",
+    weeks: "هفته",
+    months: "ماه ها",
+    years: "سال ها",
+
+    /* message popup */
+    message_ok: "تایید",
+    message_cancel: "لغو"
+  }
+};
